cb6c79fdb5a80788fae5189f948ba8b604cdfa7f
Jonathan Melly <jonathan.melly@gmail.com>
2017-12-16 12:24:20 +0100

    first attempt sqlite + ef...

---
+++ src/CalendarGenerator/Repository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarGenerator
+{
+    public class Repository
+    {
+        private const string DB_FILE = "calendar.db";
+        private SQLiteConnection connection;
+        public void Init()
+        {
+            connection = new SQLiteConnection("Data Source=" + DB_FILE, false);
+            connection.Open();
+
+            using (entity.mainEntities1 ef = new entity.mainEntities1())
+            {
+                ef.image.First();
+            }
+        }
+
+        public void Shutdown()
+        {
+            connection.Close();
+        }
+
+        
+
+    }
+}
---
+++ src/CalendarGeneratorTests/RepositoryTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalendarGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalendarGenerator.Tests
+{
+    [TestClass()]
+    public class RepositoryTests
+    {
+        Repository repository;
+
+        [TestInitialize]
+        public void Prepare()
+        {
+            repository = new Repository();
+        }
+
+        [TestMethod()]
+        public void InitTest()
+        {
+            //ARRANGE
+
+
+            //ACT
+            repository.Init();
+
+           //ASSERT
+        }
+    }
+}
